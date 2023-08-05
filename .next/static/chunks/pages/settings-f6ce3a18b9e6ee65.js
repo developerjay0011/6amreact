5c110ab8c076ff69bf57dde9ac4e4c0ebbf3100d
developerjay0011 <86921677+developerjay0011@users.noreply.github.com>
2023-08-05 11:19:23 +0530

    commit 2023-08-05

--- .next/static/chunks/pages/settings-f6ce3a18b9e6ee65.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[662],{79655:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/settings",function(){return u(86487)}])}},function(n){n.O(0,[1255,2506,6454,5269,580,7886,6487,9774,2888,179],function(){return n(n.s=79655)}),_N_E=n.O()}]);
